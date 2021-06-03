a8e9304500813cca2d86a060d7d403b9d5736cb7
Patrick Heil <theelectromagneticspectrum@gmail.com>
2021-06-03 11:28:21 -0400

    classes added

---
+++ Capstone/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public class Product
+    {
+        public string SlotId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
